848485981fc81fd1a04c846c337562f18816386b
Jevonnissocoolman <jevonnsivels2@gmail.com>
2025-04-21 22:45:24 -0700

    Update en-us.lua

--- talisman_localization/en-us.lua
+++ talisman_localization/en-us.lua
@@ -1,3 +1,20 @@
 return {
         test = "j"
+        talisman_vanilla = "Vanilla (e308)",
+        talisman_bignum = "BigNum (ee308)",
+        talisman_omeganum = "OmegaNum",
+        
+        talisman_string_A = "Select features to enable:",
+        talisman_string_B = "Disable Scoring Animations",
+        talisman_string_C = "Score Limit (requires game restart)",
+        talisman_string_D = "Calculating...",
+        talisman_string_E = "Abort",
+        talisman_string_F = "Elapsed calculations: ",
+        talisman_string_G = "Cards yet to score: ",
+        talisman_string_H = "Calculations last played hand: ",
+        talisman_string_I = "Unknown",
+
+        talisman_error_B = "[Talisman] Error unpacking string: ",
+        talisman_error_C = "[Talisman] Error loading string: ",
+        --localize("talisman_string_E")
 }
